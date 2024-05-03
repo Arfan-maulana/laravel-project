08d62ba7f30a13b514a12486bd7959d56904c1f1
Arfan-maulana <114915872+Arfan-maulana@users.noreply.github.com>
2024-05-03 18:34:22 +0700

    update

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -9,26 +9,22 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form method="post" action="{{ route('todo.store') }}" class="">
+                    <form method="post" action="{{ route('category.store') }}" class="">
                         @csrf
                         @method('post')
                         <div class="mb-6">
                             <x-input-label for="title" :value="__('Title')" />
-                            <x-text-input id="title" name="title" type="text" class="block w-full mt-1"
-                                required autofocus autocomplete="title" />
+                            <x-text-input id="title" name="title" type="text" class="block w-full mt-1" required autofocus autocomplete="title" />
                             <x-input-error class="mt-2" :messages="$errors->get('title')" />
                         </div>
                         <div class="flex items-center gap-4">
                             <x-primary-button>{{ __('Save') }}</x-primary-button>
-                            <a href="{{ route('category.index') }}"
-                                class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-gray-700 uppercase transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm dark:bg-gray-800 dark:border-gray-500 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25">
-                                {{ __('Cancel') }}
-                            </a>
+                            <a href="{{ route('category.index') }}" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-gray-700 uppercase transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm dark:bg-gray-800 dark:border-gray-500 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25">{{ __('Cancel') }}</a>
                         </div>
                     </form>
                 </div>
+
             </div>
         </div>
     </div>
 </x-app-layout>
-
--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -6,18 +6,19 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="mx-auto max-w-7x1 sm:px-6 1g:px-8">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form method="post" action="{{ route('todo.update', $category) }}" class="">
+                    <form method="post" action="{{ route('category.update', $category) }}" class="">
                         @csrf
                         @method('patch')
+
                         <div class="mb-6">
                             <x-input-label for="title" :value="__('Title')" />
-                            <x-text-input id="title" category="title" type="text" class="block w-full mt-1"
-                                :value="old('name', $category->title)" required autofocus autocomplete="title" />
+                            <x-text-input id="title" name="title" type="text" class="block w-full mt-1" :value="old('title', $category->title)" required autofocus autocomplete="title" />
                             <x-input-error class="mt-2" :messages="$errors->get('title')" />
                         </div>
+
                         <div class="flex items-center gap-4">
                             <x-primary-button>{{ __('Save') }}</x-primary-button>
                             <x-cancel-button href="{{ route('category.index') }}" />
